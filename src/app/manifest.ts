bbaa56b63467b2084eb85612a66f0551d1f6cd12
qvinh8726 <qvinh8726@users.noreply.github.com>
2026-06-09 16:12:59 +0700

    add-maskable-icon-for-android-adaptive

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,11 +15,19 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
       },
       {
         src: "/icon.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
